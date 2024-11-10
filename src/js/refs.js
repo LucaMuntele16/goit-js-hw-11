b883b1276aeaac269ec475b15cfa91f4c9f264fb
LucaMuntele16 <119846365+LucaMuntele16@users.noreply.github.com>
2024-11-10 16:31:14 +0200

    update

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export const refs = {
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.section-gallery'),
+  btnLoadMore: document.querySelector('.load-more'),
+  searchInput: document.querySelector('.form-input'),
+  loaderElement: document.querySelector('.loader-spin'),
+};
